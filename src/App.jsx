f0ed785c30ff0886fc370c18ad4b17df25da5cfc
wkauan <kauanmbomfim5@gmail.com>
2023-09-20 10:51:32 -0300

    :tada: feat: Iniciando o projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div className="bg-gray-300 h-screen w-full">
+        <div>
+          <h1
+            className='text-3xl bg-blue-200'>
+            Hello World
+          </h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
