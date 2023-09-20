a65a33d558015bb21a430f20e842ea90c3be4f47
wkauan <kauanmbomfim5@gmail.com>
2023-09-20 13:59:27 -0300

    rafactor: :recycle: Removendo a rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { Route, BrowserRouter } from "react-router-dom";
-
 import Page from "./components/page"
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Route Component={ Page } path="/" exact />
-      </BrowserRouter>
+      <Page />
     </>
   )
 }
